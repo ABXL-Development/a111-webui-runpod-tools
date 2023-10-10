e12e3173aec72014de9d5dd61a3a60ae29cfef8e
pptdd <146601929+pptdd@users.noreply.github.com>
2023-10-10 12:34:29 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,11 @@ wget -O ABXLv0.2.5.safetensors https://civitai.com/api/download/models/179489
 #ABXLv0.1
 #wget -O ABXLv0.1.safetensors https://civitai.com/api/download/models/173095
 
+#Upscalers
+cd /workspace/stable-diffusion-webui/models/ESRGAN
+
+wget -O 4xUltrasharpV10.pt https://civitai.com/api/download/models/125843
+
 #Checkpoints
 cd /workspace/stable-diffusion-webui/models/Stable-diffusion
 
